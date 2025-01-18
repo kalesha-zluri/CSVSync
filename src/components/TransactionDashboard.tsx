e684c9d47c15557a6da2a390d7921803cb567531
shaik-zluri <shaik.k@zluri.com>
2025-01-19 00:40:23 +0530

    implement transaction fetching and loading state in TransactionDashboard component

--- src/components/TransactionDashboard.tsx
+++ src/components/TransactionDashboard.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
+import { Transaction, TransactionResponse } from "../types";
+import { api } from "../api";
+import toast from "react-hot-toast";
 
-const TransactionDashboard = () => {
-  return (
-    <div>
-      <h1>Transaction Dashboard</h1>
-    </div>
-  );
+export function TransactionDashboard() {
+  const [loading, setLoading] = useState(true);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [pagination, setPagination] = useState({
+    currentPage: 1,
+    totalPages: 1,
+    totalCount: 0,
+  });
+  const [showForm, setShowForm] = useState(false);
+  const [selectedTransaction, setSelectedTransaction] = useState<
+    Transaction | undefined
+  >();
+
+  const fetchTransactions = async (page: number = 1) => {
+    try {
+      setLoading(true);
+      const response: TransactionResponse = await api.getTransactions(page);
+      setTransactions(response.transactions);
+      setPagination({
+        currentPage: response.currentPage,
+        totalPages: response.totalPages,
+        totalCount: response.totalCount,
+      });
+    } catch (error) {
+      console.error("Fetch error", error);
+      toast.error("Failed to fetch transactions");
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  return <> </>;
 }
-
-export { TransactionDashboard };
